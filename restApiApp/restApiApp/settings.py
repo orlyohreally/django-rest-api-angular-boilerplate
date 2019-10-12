edbc554056c0d334d7e34b2a55335a25dce03ca8
orlyohreally <orli-knop@ya.ru>
2019-10-12 20:33:13 +0300

    Prepared for heroku

--- restApiApp/restApiApp/settings.py
+++ restApiApp/restApiApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,3 +130,6 @@ STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), "static_cdn")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+
+django_heroku.settings(locals())
